b4abdb69e3401c47cdb0f199e0c4be0871904938
TheOriginalKyle <2002kbohlander@gmail.com>
2018-03-05 21:22:33 -0800

    Validated Input

--- Public/register.php
+++ Public/register.php
@@ -10,16 +10,77 @@
  $db = DBAccess::getMysqliConnection();
 
 // Set session variables to be used on profile.php page
+
+// Validate Input
+$dirtyEmail = $_POST['email'];
+$dirtyFirstName = $_POST['firstname'];
+$dirtyLastName = $_POST['lastname'];
+$dirtyPassword = $_POST['password'];
+
+if(has_presence($dirtyEmail) && has_presence($dirtyFirstName) && has_presence($dirtyLastName) && has_presence($dirtyPassword))
+{
+  if(has_length(trim($dirtyEmail), 'min' => 3, 'max' => 50))
+  {
+    $noHTMLemail = h(trim($dirtyEmail);
+    $cleanEmail = mysqli_real_escape_string($noHTMLemail);
+
+  }else{
+    $_SESSION['message'] = 'Thats not a real email.';
+    header("location: error.php");
+  }
+
+  if(has_length(trim($dirtyFirstName), 'min' => 2, 'max' => 50))
+  {
+    $noHTMLfirstName = h(trim($dirtyFirstName));
+    $cleanFirstName = mysqli_real_escape_string($noHTMLfirstName);
+  }else{
+    $_SESSION['message'] = 'Your name is too short.';
+    header("location: error.php");
+  }
+
+  if(has_length(trim($dirtyLastName), 'min' => 3, 'max' => 50))
+  {
+    $noHTMLlastName = h(trim($dirtyLastName));
+    $cleanLastName = mysqli_real_escape_string($noHTMLlastName);
+  }else{
+    $_SESSION['message'] = 'Your name is too short.';
+    header("location: error.php");
+  }
+
+  if(has_length(trim($dirtyPassword), 'min' => 8))
+  {
+    if(has_format_matching(trim($dirtyPassword), '/[^A-Za-z0-9]/'))
+    {
+      $validPassword = trim($dirtyPassword);
+      $cleanPassword = mysqli_real_escape_string($db,password_hash($validPassword, PASSWORD_BCRYPT));
+      $hash = mysqli_real_escape_string( $db,md5( rand(0,1000) ) );
+
+    }else{
+      $_SESSION['message'] = 'Your password is not strong enought.';
+      header("location: error.php");
+    }
+
+  }else{
+    $_SESSION['message'] = 'Your password is not strong enough.';
+    header("location: error.php");
+  }
+}else
+{
+  $_SESSION['message'] = 'You forgot to enter something in!';
+  header("location: error.php");
+}
+
 $_SESSION['email'] = $_POST['email'];
 $_SESSION['first_name'] = $_POST['firstname'];
 $_SESSION['last_name'] = $_POST['lastname'];
 
+
 // Escape all $_POST variables to protect against SQL injections
-$first_name = mysqli_real_escape_string($db,$_POST['firstname']);
-$last_name = mysqli_real_escape_string($db,$_POST['lastname']);
-$email = mysqli_real_escape_string($db,$_POST['email']);
-$password = mysqli_real_escape_string($db,password_hash($_POST['password'], PASSWORD_BCRYPT));
-$hash = mysqli_real_escape_string( $db,md5( rand(0,1000) ) );
+//$first_name = mysqli_real_escape_string($db,$_POST['firstname']);
+//$last_name = mysqli_real_escape_string($db,$_POST['lastname']);
+//$email = mysqli_real_escape_string($db,$_POST['email']);
+//$password = mysqli_real_escape_string($db,password_hash($_POST['password'], PASSWORD_BCRYPT));
+//$hash = mysqli_real_escape_string( $db,md5( rand(0,1000) ) );
 // $first_name = $mysqli->escape_string($_POST['firstname']);
 // $last_name = $mysqli->escape_string($_POST['lastname']);
 // $email = $mysqli->escape_string($_POST['email']);
